807a1fb98bd933d2d97c333d1dc3e5152ad53de7
Bruno Miiller <55815555+miillerb@users.noreply.github.com>
2021-08-20 17:02:57 -0300

    Add chocolatey script via upload

---
+++ install-choco-script-2.bat
@@ -0,0 +1,35 @@
+:: Step 2: Install all the packages
+
+:: Browsers
+choco install brave -fy
+choco install vivaldi -fy
+
+:: Dev tools
+#choco install git -fy
+#choco install nodejs.install -fy
+
+:: Education
+choco install zoom -fy
+
+:: Text editors / IDEs
+#choco install visualstudiocode -fy
+choco install obsidian
+
+:: Media
+choco install vlc -fy
+
+:: Tools
+choco install powertoys -fy
+choco install eartrumpet -fy
+choco install modernflyouts -fy
+choco install microsoft-windows-terminal -fy
+
+:: Social Media
+choco install telegram -fy
+
+:: Suits Office
+#choco install office365proplus -fy
+
+:: Utilities and others
+choco install revo-uninstaller -fy
+choco install evernote -fy
