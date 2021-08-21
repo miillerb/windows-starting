59b627b379e1dc261edf1d0e7754f2c351143c45
Bruno Miiller <55815555+miillerb@users.noreply.github.com>
2021-08-21 09:32:51 -0300

    Update install-choco-script.bat

--- install-choco-script.bat
+++ install-choco-script.bat
@@ -1,29 +1,3 @@
 :: Step 1: Install choco.exe and add choco to PATH
 
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-:: Step 2: Install all the packages
-
-:: Browsers
-choco install brave -fy
-choco install vivaldi -fy
-
-:: Education
-choco install zoom
-
-:: Text editors / IDEs
-#choco install visualstudiocode -fy
-
-:: Dev tools
-#choco install git -fy
-#choco install nodejs.install -fy
-
-:: Media
-choco install vlc -fy
-
-:: Utilities and others
-choco install revo-uninstaller -fy
-choco install powertoys -fy
-choco install eartrumpet -fy
-choco install evernote -fy
-#choco install office365proplus -fy
